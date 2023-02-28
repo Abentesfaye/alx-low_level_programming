cad916c0743cf3b0842ff9f84a2b0717f38922b1
Abentesfaye <abentesfaye11@gmail.com>
2023-02-27 23:29:40 -0500

    final commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,13 @@
 void rev_string(char *s)
 {
 	int len = 0;
-    while (s[len] != '\0') 
+	while (s[len] != '\0') 
 	{  
-        len++;
-    }
-    for (int i = 0; i < len / 2; i++) {
-        char temp = s[i];
-        s[i] = s[len - i - 1];
-        s[len - i - 1] = temp;
-    }
+		len++;
+	}
+	for (int i = 0; i < len / 2; i++) {
+		char temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
 }
