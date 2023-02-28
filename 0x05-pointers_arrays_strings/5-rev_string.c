5765234cb61d00eb7696cab5c7b6816b99d0c7d2
Abentesfaye <abentesfaye11@gmail.com>
2023-02-27 23:11:47 -0500

    change to five

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,4 @@
-#include "main.h""
+#include "main.h"
 
 /**
  * rev_string - reverse a string
@@ -9,17 +9,14 @@
 
 void rev_string(char *s)
 {
-	int i, c, l;
-	char h;
-
-	for (i = 0; s[i] != '\0'; i++)
-		;
-
-	l = i;
-	for (i--, c = 0; c < l / 2; i--, c++)
-	{
-		h = s[c];
-		s[c] = s[i];
-		s[i] = h;
-	}
+	int len = 0;
+    while (s[len] != '\0') 
+	{  
+        len++;
+    }
+    for (int i = 0; i < len / 2; i++) {
+        char temp = s[i];
+        s[i] = s[len - i - 1];
+        s[len - i - 1] = temp;
+    }
 }
