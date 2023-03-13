88a1e8aa390568bdd0d7f39638cebbddd7c1a912
Abentesfaye <abentesfaye11@gmail.com>
2023-03-13 04:43:41 -0700

    initila commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * create_array - function return pointer to allocated memory
+ * @size : parametr for the size of memory to be allocat
+ * @c : a character that going to be assigned to the memory i
+ * Return: char pointer to dest
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	       str = (char *) malloc(size);
+	return (str);
+}
+
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+# dynamic memory allocating using malloc function
