d25369928932d833411d4e5ca53b3396804360f4
Abentesfaye <abentesfaye11@gmail.com>
2023-03-13 05:30:23 -0700

    initial commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 {
 	if (size == 0)
 		return (NULL);
-	char array;
+	char *array;
 
 	array = (char *) malloc(size * sizeof(char));
 	if (array == NULL)
