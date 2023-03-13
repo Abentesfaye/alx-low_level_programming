b68a6cd92b81941c437688553c856b79771f0654
Abentesfaye <abentesfaye11@gmail.com>
2023-03-13 11:29:31 -0700

    fun

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "main.h"
 /*
- * _strdup -  copy the variable and allocat new memory
+ *_strdup -  copy the variable and allocat new memory
  * @str : string that going to be copy
  * Return : pointer to the new stirng
  */
@@ -10,13 +10,17 @@ char *_strdup(char *str)
 {
 	char *newstr;
 	int i;
+	int lenght = 0;
+
+	while (str[length] != '\0')
+		lenght++;
 
 	if (str == NULL)
 		return (NULL);
 
 	newstr = malloc(sizeof(str));
 
-	for (i = 0; i < strlen(str); i++)
+	for (i = 0; i < length; i++)
 	{
 		newstr[i] = str[i];
 	}
