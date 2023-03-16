e50572323ce5bc4915c742a6a1fe11756a3041a5
Abentesfaye <abentesfaye11@gmail.com>
2023-03-16 02:15:23 -0700

    unuse

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main -> starting point of the program
- * @argc : argumet counter for the command line 
+ * @argc : argumet counter for the command line
  * @argv : argumet vector of the commad line
  * Return: nothing
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	int i;
-	
+
 	for (i = 0; i <= argc; i++)
 	{
 		printf("%i\n", i);
