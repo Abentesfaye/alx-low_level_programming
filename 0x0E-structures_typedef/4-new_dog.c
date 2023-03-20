10e90109312ad84554ea7e449c22a9aec17acf37
Abentesfaye <abentesfaye11@gmail.com>
2023-03-20 13:45:58 -0700

    hot fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,46 +1,81 @@
 #include "dog.h"
 /**
+ * _strlen - returns the length of a string
+ * @s: String to count that pass to the function
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
+/**
+  * _strcpy - Copy a string
+  * @dest: Destination value
+  * @src: Source value
+  *
+  * Return: the pointer to dest
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - create new dog
  * @name: name of new dog
- * @age age of new dog
+ * @age: age of new dog
  * @owner: owner of new dog
  * Return: new dog
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
-	int i, nameLength = 0, ownerLength = 0;
 
-	new = malloc(sizeof(*new));
+	new = malloc(sizeof(dog_t));
 
 	if (new == NULL || name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
 
-	while (name[nameLength] != '\0')
-		nameLength++;
+	new->name = malloc(sizeof(char) * (_strlen(name) + 1));
 
-	while (owner[ownerLength] != '\0')
-		ownerLength++;
-
-	new->name = malloc(nameLength + 1);
-
-	new->owner = malloc(ownerLength + 1);
-
-	if (new->name == NULL || new->owner == NULL)
+	if (new->name == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 
-	for (i = 0; i < nameLength; i++)
-		new->name[i] = name[i];
-
-	new->name[i] = '\0';
+	new->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (new->owner == NULL)
+	{
+		free(new);
+		free(new->name);
+		return (NULL);
+	}
+	new->name = _strcpy(new->name, name);
 	new->age = age;
-
-	for (i = 0; i < ownerLength; i++)
-		new->owner[i] = owner[i];
-
-	new->owner[i] = '\0';
+	new->owner = _strcpy(new->owner, owner);
 
 	return (new);
 }
