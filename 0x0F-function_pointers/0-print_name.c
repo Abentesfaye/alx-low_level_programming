1fc14367fc53e690cd8034a495d8cca88c39a6ed
Abentesfaye <abentesfaye11@gmail.com>
2023-03-22 06:16:33 -0700

    initial commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name- prints a name
- * @name: name to  be prit
+ * @name: name to  be print
  * @f: function to be called
  * Return: nothing
  */
@@ -10,5 +11,9 @@ void print_name(char *name, void (*f)(char *))
 	if (name != NULL && f != NULL)
 	{
 		f(name);
+	}
+	else
+	{
+		return;
 	}
 }
