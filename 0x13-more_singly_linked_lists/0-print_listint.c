e436beb9509f95e404b5d9b10d717c25676ba5fe
Abentesfaye <abentesfaye11@gmail.com>
2023-04-04 12:11:09 -0700

    initial commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,10 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- *print_listint - print list off the node 
- *@h: the value to be print
- *Return: length of the list 
+ *print_listint - print list off the node
+ *@h:the value to be print
+ *Retur0n:length of the list
  */
 
 size_t print_listint(const listint_t *h)
