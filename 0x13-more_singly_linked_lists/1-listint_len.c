028b093c7bca23045ae6f6bf7c9eb4a76aa90599
Abentesfaye <abentesfaye11@gmail.com>
2023-04-04 12:32:53 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+include "lists.h"
+/**
+ * listint_len -  function to count legth of the list
+ * @h:pointer to the list
+ * Return: length of the list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
