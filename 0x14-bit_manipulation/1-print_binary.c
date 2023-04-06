2fc3b5767171f9f302457b0e699eb5c54dee63b4
Abentesfaye <abentesfaye11@gmail.com>
2023-04-06 12:25:08 -0700

    task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: unsigned long int to convert and prints
+ *
+ * Return: nothing
+ */
+void Print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) ? '1' : '0');
+}
---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to the standard output stream
+ * @c: character to be written
+ *
+ * Return: On success, returns the character written as an unsigned char.
+ *         On error, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
